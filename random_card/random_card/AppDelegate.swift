59a7837a5ea08677d53d29f54ccafb89b068d440
Jennifer Anderson <jennifer@macpractice.com>
2016-01-25 08:19:49 -0600

    Started the random card app

---
+++ random_card/random_card/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  random_card
+//
+//  Created by Jennifer Loecker on 1/25/16.
+//  Copyright © 2016 Jennifer Anderson. All rights reserved.
+//
+/*!
+
+@class AppDelegate.swift
+
+@abstract Starts the Random Card app
+
+@updated 2016-01-25
+
+@discussion
+Starts window with the random card showing.  This is a simple app to help build my card framework and test it before
+using
+
+Todo:
+
+Call window to show the random card
+Might need to reset card properties on close
+Add tests
+
+*/
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+	
+	@IBOutlet weak var window: NSWindow!
+	
+	
+	func applicationDidFinishLaunching(aNotification: NSNotification)
+	{
+		// Insert code here to initialize your application
+	}
+	
+	func applicationWillTerminate(aNotification: NSNotification)
+	{
+		// Insert code here to tear down your application
+	}
+	
+}
